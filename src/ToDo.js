20d3fc2205f63ad06bf2a187afe5df5ffb88f62d
Orlando Verdin <hello@orli.land>
2022-05-22 18:32:08 -0600

    Main features(create, check/uncheck, delete task)

---
+++ src/ToDo.js
@@ -0,0 +1,79 @@
+import { React, Component } from "react";
+import Form from "./Components/Form";
+import ItemsList from "./Components/ItemsList";
+import DoneItems from "./Components/DoneItems";
+
+class ToDo extends Component {
+  initialInputTask = "";
+  state = {
+    inputTask: this.initialInputTask,
+    tasksToDo: [],
+  };
+
+  // Method that listen the changes of the input for new tasks
+  handleChange = (event) => {
+    const { value } = event.target;
+    this.setState({
+      inputTask: value,
+    });
+  };
+
+  // Method that add new task to the list
+  handleAddTask = (event) => {
+    const { inputTask, tasksToDo } = this.state;
+    const data = {
+      title: inputTask,
+      done: false,
+    };
+    this.setState({
+      tasksToDo: [...tasksToDo, data],
+      inputTask: this.initialInputTask,
+    });
+    event.preventDefault();
+  };
+
+  // Method that check or uncheck if a task is completed
+  handleCheckTask = (index, check) => {
+    const { tasksToDo } = this.state;
+
+    tasksToDo[index].done = check;
+    this.setState({
+      tasksToDo: tasksToDo,
+    });
+  };
+
+  // Method that delete a task from the list
+  handleDeleteTask = (index) => {
+    const { tasksToDo } = this.state;
+
+    this.setState({
+      tasksToDo: tasksToDo.filter((task, i) => {
+        return i !== index;
+      }),
+    });
+  };
+
+  render() {
+    const { inputTask, tasksToDo } = this.state;
+    return (
+      <div>
+        <Form
+          handleChange={this.handleChange}
+          handleAddTask={this.handleAddTask}
+          value={inputTask}
+        />
+        <ItemsList
+          tasksToDo={tasksToDo}
+          handleCheckTask={this.handleCheckTask}
+          handleDeleteTask={this.handleDeleteTask}
+        />
+        <DoneItems
+          tasksToDo={tasksToDo}
+          handleUnCheckTask={this.handleCheckTask}
+        />
+      </div>
+    );
+  }
+}
+
+export default ToDo;
